4b6f5faca49fa17d69b725912af69ac5737e926e
Guudz <Kilian1702@outlook.com>
2023-01-27 20:40:52 +0100

    Initial commit

---
+++ kartingjob/config.lua
@@ -0,0 +1,30 @@
+Config                        = {}
+Config.DrawDistance           = 100.0
+Config.MaxInService           = -1
+Config.EnablePlayerManagement = true
+Config.Locale                 = 'fr'
+
+Config.Zones = {
+
+	KartingActions = {
+		Pos   = {x = -163.03, y = -2130.48, z = 16.71},
+		Size  = {x = 1.5, y = 1.5, z = 1.0},
+		Color = {r = 204, g = 204, b = 0},
+		Type  = 22
+	},
+
+	VehicleDeleter = {
+		Pos   = {x = -162.92, y = -2134.8, z = 15.71},
+		Size  = {x = 1.5, y = 1.5, z = 1.0},
+		Color = {r = 102, g = 0, b = 0 },
+		Type  = 1
+	},
+
+	VehicleSpawnPoint = {
+		Pos   = {x = -161.67, y = -2138.16, z = 16.71},
+		Size  = {x = 3.0, y = 3.0, z = 1.0},
+		Color = {r = 204, g = 0, b = 0},
+		Type  = -1
+	},
+
+}
